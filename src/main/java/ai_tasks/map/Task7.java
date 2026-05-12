9cd2ed70c66dd5040bf6861b158ab5c0d39608ed
Barsukov Sergey <BraveBars@gmail.com>
2026-05-12 03:32:24 +0300

    add AI Task7

---
+++ src/main/java/ai_tasks/map/Task7.java
@@ -0,0 +1,34 @@
+package ai_tasks.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//У тебя есть строка:
+//String s = "aabbccddee";
+//Напиши код который проверит является ли строка анаграммой другой строки:
+//String t = "abcdeabcde";
+//Анаграмма — это когда две строки содержат одинаковые буквы одинаковое количество раз, но в разном порядке.
+//Примеры:
+//
+//"abc" и "cba" → анаграммы
+//"abc" и "cbb" → не анаграммы
+public class Task7 {
+    public static void main(String[] args) {
+        String s = "aabbccddee";
+        String t = "abcdeabcde";
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (Character ch: s.toCharArray()){
+            sMap.put(ch, sMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (Character ch: t.toCharArray()){
+            tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
+        }
+
+        System.out.println(sMap);
+        System.out.println(tMap);
+        System.out.println(sMap.equals(tMap));
+    }
+}
